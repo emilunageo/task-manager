2a98445ae2c6c1329de85438216fb69f78ca3ea5
Emiliano Luna George <48260618+emilunageo@users.noreply.github.com>
2022-10-07 18:50:31 -0500

    I complete the delate_tasks function

--- main.py
+++ main.py
@@ -42,20 +42,20 @@ def show_tasks(tasks):
 def delate_tasks():
     print("To delate a task write the the number the task.")
     print("If you want to stop delating tasks write: exit")
+    show_tasks(tasks)
     while True:
-        show_tasks(tasks)
         input_task = input("Write the number of task that you want to remove: ")
         if input_task != "exit":
             input_task = int(input_task)
-            if input_task > len(tasks) + 1 or input_task <= 0:
+            if input_task > len(tasks) or input_task <= 0:
                 print("The number of task you write is not valid please try again")
             else:
-                task_want_to_remove = tasks.index(input_task)
+                task_want_to_remove = tasks[input_task - 1]
                 print(f"Are you sure you want to delate this task: {task_want_to_remove} \n")
                 decision = input("y/n to conitune: ")
                 if decision == "y":
                     tasks.remove(task_want_to_remove)
-                    show_tasks(tasks)
+                    print(f"This are your new list of tasks: \n {tasks}")
                 else:
                     print("ok")
                     break

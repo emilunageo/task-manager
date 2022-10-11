6a7127c95bc4129818af7e2a2b8d51784ca487c2
Emiliano Luna George <48260618+emilunageo@users.noreply.github.com>
2022-10-10 23:09:31 -0500

    Improving the program and solving some error cases

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-# Start of the program
-from http.client import OK
-
-
 print("""Select the option you want to run \n
-1. Add taks\n
-2. Show tasks\n
-3. Delate tasks\n
+1. Add taks
+2. Show tasks
+3. Delate tasks
 4. Exit\n""")
 opcion = int(input(("Write the option: ")))
 
 # A print menu function
 def printMenu():
-    print("""Select the option you want to run \n
-    1. Add taks\n
-    2. Show tasks\n
-    3. Delate tasks\n
-    4. Exit\n""")
+    print("""----------------------------------
+\nSelect the option you want to run \n
+1. Add taks
+2. Show tasks
+3. Delate tasks
+4. Exit\n
+----------------------------------""")
     opcion = int(input(("Write the option: ")))
     return opcion
 
@@ -32,36 +30,47 @@ def add_tasks():
             break
 # Show tasks
 def show_tasks(tasks):
-    count = 0
-    print("This are your tasks: ")
-    for task in tasks:
-        count += 1
-        print(f"\n{count}. {task}\n")
-
+    if tasks != []:
+        count = 0
+        print("This are your tasks: \n")
+        for task in tasks:
+            count += 1
+            print(f"{count}. {task}")
+        print("")
+    else:
+        print("---- You didn't add any task yet or you already delate all your tasks, please add some to the program ----")
 # Delate tasks
 def delate_tasks():
-    print("To delate a task write the the number the task.")
-    print("If you want to stop delating tasks write: exit")
-    show_tasks(tasks)
-    while True:
-        input_task = input("Write the number of task that you want to remove: ")
-        if input_task != "exit":
-            input_task = int(input_task)
-            if input_task > len(tasks) or input_task <= 0:
-                print("The number of task you write is not valid please try again")
-            else:
-                task_want_to_remove = tasks[input_task - 1]
-                print(f"Are you sure you want to delate this task: {task_want_to_remove} \n")
-                decision = input("y/n to conitune: ")
-                if decision == "y":
-                    tasks.remove(task_want_to_remove)
-                    print(f"This are your new list of tasks: \n {tasks}")
+    if tasks != []:
+        print("To delate a task write the number of the task.")
+        print("If you want to stop delating tasks write: exit")
+        show_tasks(tasks)
+        while True:
+            input_task = input("Write the number of task that you want to remove: ")
+            if input_task != "exit":
+                input_task = int(input_task)
+                if input_task > len(tasks) or input_task <= 0:
+                    print("The number of task you write is not valid please try again")
                 else:
-                    print("ok")
-                    break
-        else:
-            print("ok")
-            break
+                    task_want_to_remove = tasks[input_task - 1]
+                    print(f"Are you sure you want to delate this task: {task_want_to_remove}")
+                    decision = input("y/n to conitune: ")
+                    if decision == "y":
+                        tasks.remove(task_want_to_remove)
+                        print(show_tasks(tasks))
+                        if tasks != []:
+                            continue
+                        else:
+                            print("\n---- You already delate every single task in the program, please add new tasks to the program ----\n")
+                            break
+                    else:
+                        print("ok")
+                        break
+            else:
+                print("ok")
+                break
+    else:
+        print(show_tasks(tasks))
 # A while loop that allow to get back to the main menu
 while opcion != 4:
     if opcion == 1:
@@ -71,4 +80,3 @@ while opcion != 4:
     if opcion == 3:
         delate_tasks()
     opcion = printMenu()
-

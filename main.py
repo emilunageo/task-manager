cde1b3de1dfe134b6aeee38612d56a48c3c1006f
Emiliano Luna George <48260618+emilunageo@users.noreply.github.com>
2022-10-20 17:57:33 -0500

    Hot fix none error and adding the edit_task function
    Fix the "none" error that was appearing in the function delate_task(). This happened because the print was called twice, this means that in the show_tasks() function it already printed the tasks and I sent the function to print:
    print(show_tasks())
    That's why there was the error appeared.
    
    I also created the edit_tasks() function which is similar to the delete_tasks() function, but this function allows you to edit already existing tasks.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+print("""Welcome to task manager pro (for pros)""")
 print("""Select the option you want to run \n
 1. Add taks
 2. Show tasks
 3. Delate tasks
-4. Exit\n""")
+4. Edit tasks
+5. Exit\n""")
 opcion = int(input(("Write the option: ")))
-
 # A print menu function
 def printMenu():
     print("""----------------------------------
@@ -12,13 +13,12 @@ def printMenu():
 1. Add taks
 2. Show tasks
 3. Delate tasks
-4. Exit\n
+4. Edit tasks
+5. Exit\n
 ----------------------------------""")
     opcion = int(input(("Write the option: ")))
     return opcion
-
 tasks = []
-delating_list = []
 # Create tasks:
 def add_tasks():
     print("If you want to stop adding tasks write: exit")
@@ -54,10 +54,10 @@ def delate_tasks():
                 else:
                     task_want_to_remove = tasks[input_task - 1]
                     print(f"Are you sure you want to delate this task: {task_want_to_remove}")
-                    decision = input("y/n to conitune: ")
+                    decision = input("to conitune write yes or no (y/n): ")
                     if decision == "y":
                         tasks.remove(task_want_to_remove)
-                        print(show_tasks(tasks))
+                        show_tasks(tasks)
                         if tasks != []:
                             continue
                         else:
@@ -70,13 +70,44 @@ def delate_tasks():
                 print("ok")
                 break
     else:
-        print(show_tasks(tasks))
+        show_tasks(tasks)
+# Edit task
+def edit_tasks():
+    if tasks != []:
+        print("To edit a task write the number of the task. Then edit the task.")
+        print("If you want to stop delating tasks write: exit")
+        show_tasks(tasks)
+        while True:
+            input_task = input("Write the number of task that you want to edit: ")
+            if input_task != "exit":
+                input_task = int(input_task)
+                if input_task > len(tasks) or input_task <= 0:
+                    print("The number of task you write is not valid please try again")
+                else:
+                    task_want_to_edit = tasks[input_task - 1]
+                    print(f"Are you shure you want to edit this task: {task_want_to_edit}")
+                    decision = input("to conitune write yes or no (y/n): ")
+                    if decision == "y":
+                        task_edited = input("Edit: ")
+                        tasks[input_task - 1] = task_edited
+                        show_tasks(tasks)
+                    else:
+                        print("ok")
+                        break
+            else:
+                print("ok")
+                break
+    else:
+        show_tasks(tasks)
+         
 # A while loop that allow to get back to the main menu
-while opcion != 4:
+while opcion != 5:
     if opcion == 1:
         add_tasks()
     if opcion == 2:
         show_tasks(tasks)
     if opcion == 3:
         delate_tasks()
+    if opcion == 4:
+        edit_tasks()
     opcion = printMenu()
